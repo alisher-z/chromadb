2d1cf27cabb07f2e89740001499d01378b62c94f
AliSherZ <zabiullah.alisher@gmail.com>
2025-08-20 15:10:01 +0300

    removed extra files

--- database_orm.py
+++
@@ -1,7 +0,0 @@
-import chromadb
-from chromadb.utils import embedding_functions
-
-default_ef = embedding_functions.DefaultEmbeddingFunction()
-db = chromadb.PersistentClient('./db/persisted')
-
-collection = db.get_or_create_collection('info', embedding_function=default_ef)
